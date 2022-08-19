f3ce3f19e19b62a345bc9c854bf23c29d2e81946
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-19 09:14:48 +0530

    modified

---
+++ lib/chat/view/widgets/sender_message_card.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/utilities/colors.dart';
+import 'package:whatsapp_clone/utilities/constant.dart';
+
+class SenderMessageCard extends StatelessWidget {
+  const SenderMessageCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.width - 45,
+        ),
+        child: Card(
+          elevation: 1,
+          color: messageColor,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Stack(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  bottom: 30,
+                  right: 30,
+                  top: 10,
+                ),
+                child: Text(
+                  "Hey, I Hope you are doing great!",
+                  style: TextStyle(
+                    color: textColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 4,
+                right: 10,
+                child: Row(
+                  children: const [
+                    Text(
+                      "11:30",
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 13,
+                      ),
+                    ),
+                    sizedBoxWidth10,
+                    Icon(
+                      Icons.done_all,
+                      color: textColor,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
