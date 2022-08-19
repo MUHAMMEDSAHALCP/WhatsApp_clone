0f858b83c0edb8515ca478e68700b3f96fc6acb5
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-19 09:15:22 +0530

    modified

--- lib/home/view/widgets/contacts_list.dart
+++ lib/home/view/widgets/contacts_list.dart
@@ -35,7 +35,7 @@ class ContactList extends StatelessWidget {
                     info[index]["message"],
                   ),
                 ),
-              )
+              ),
             ],
           );
         }),
