60de605ef442838bd9cdbd572ddd969218060fde
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-19 07:45:08 +0530

    Firebase successfully initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/firebase_options.dart';
 import 'package:whatsapp_clone/home/view/mobile_layout_view.dart';
 import 'package:whatsapp_clone/home/view/web_layout_view.dart';
 import 'package:whatsapp_clone/utilities/responsive_layout.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
