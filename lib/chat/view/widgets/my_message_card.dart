edcd6671d39f7b10a2d9d4170e82a1d487277eac
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-19 09:14:30 +0530

    modified

--- lib/chat/view/widgets/my_message_card.dart
+++ lib/chat/view/widgets/my_message_card.dart
@@ -1,29 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/utilities/colors.dart';
+import 'package:whatsapp_clone/utilities/constant.dart';
 
 class MyMessageCard extends StatelessWidget {
   const MyMessageCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxHeight: MediaQuery.of(context).size.width - 3,
-      ),
-      child: Card(
-        color: messageColor,
-        child: Stack(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(
-                left: 10,
-                bottom: 20,
-                right: 30,
-                top: 5,
-              ),
-              child: Text("Hey  message"),
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.width - 45,
+        ),
+        child: Card(
+          elevation: 1,
+          color: messageColor,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
             ),
-          ],
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Stack(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  bottom: 30,
+                  right: 30,
+                  top: 10,
+                ),
+                child: Text(
+                  "Hey, I Hope you are doing great!",
+                  style: TextStyle(
+                    color: textColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 4,
+                right: 10,
+                child: Row(
+                  children: const [
+                    Text(
+                      "11:30",
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 13,
+                      ),
+                    ),
+                    sizedBoxWidth10,
+                    Icon(
+                      Icons.done_all,
+                      color: textColor,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
